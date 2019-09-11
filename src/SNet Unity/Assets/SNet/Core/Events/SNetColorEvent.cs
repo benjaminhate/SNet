1722e44292d2ee0696cd756190f1116c605d6e0a
Benjamin Haté <benjamin.hate.09@gmail.com>
2019-09-11 14:34:46 +0200

    Fixed SNetIntEvent

--- src/SNet Unity/Assets/SNet/Core/Events/SNetColorEvent.cs
+++ src/SNet Unity/Assets/SNet/Core/Events/SNetColorEvent.cs
@@ -1,4 +1,5 @@
 using System;
+using SNet.Core.Events.Serializers;
 using UnityEngine;
 using UnityEngine.Events;
 
--- src/SNet Unity/Assets/SNet/Core/Events/SNetIntEvent.cs
+++ src/SNet Unity/Assets/SNet/Core/Events/SNetIntEvent.cs
@@ -1,48 +1,39 @@
-﻿//using System;
-//using UnityEngine.Events;
+﻿using System;
+using SNet.Core.Events.Serializers;
+using UnityEngine.Events;
 
-//namespace SNet.Core.Events
-//{
-//    [Serializable] public class IntEvent : UnityEvent<int> { }
+namespace SNet.Core.Events
+{
+    [Serializable] public class IntEvent : UnityEvent<int> { }
 
-//    public class SNetIntEvent : SNetEvent<int>
-//    {
-//        public IntEvent clientReceiveCallback;
-//        public IntEvent serverReceiveCallback;
+    public class SNetIntEvent : SNetEvent
+    {
+        public IntEvent clientReceiveCallback;
+        public IntEvent serverReceiveCallback;
 
-//        private new void Awake()
-//        {
-//            clientReceive = clientReceiveCallback;
-//            base.Awake();
-//        }
+        protected override void Setup()
+        {
+            clientReceive += OnClientReceive;
+            serverReceive += OnServerReceive;
+        }
 
-//        public new void ClientSend(byte[] data)
-//        {
-//            throw new NotImplementedException();
-//        }
-
-//        public new void OnClientReceive(byte[] array)
-//        {
-//            clientReceiveCallback?.Invoke(SNetIntSerializer.Deserialize(array));
-//        }
-
-//        public void ServerBroadcast(int data)
-//        {
-//            var value = BitConverter.GetBytes(data);
-//            if (BitConverter.IsLittleEndian)
-//                Array.Reverse(value);
-
-//            ServerBroadcastSerializable(value);
-//        }
+        private void OnClientReceive(uint peerId, byte[] array)
+        {
+            clientReceiveCallback?.Invoke(SNetIntSerializer.Deserialize(array));
+        }
         
-//        public new void ServerSend(object target, byte[] data)
-//        {
-//            throw new NotImplementedException();
-//        }
+        private void OnServerReceive(uint peerId, byte[] array)
+        {
+            serverReceiveCallback?.Invoke(SNetIntSerializer.Deserialize(array));
+        }
 
-//        public new void OnServerReceive(byte[] array)
-//        {
-//            serverReceiveCallback?.Invoke(SNetIntSerializer.Deserialize(array));
-//        }
-//    }
-//}
+        public void ServerBroadcast(int data)
+        {
+            var value = BitConverter.GetBytes(data);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(value);
+
+            ServerBroadcastSerializable(value);
+        }
+    }
+}
